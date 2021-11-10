36d076dea69cafabe836f2fc09cbab4fa7f55ec0
Zen8688 <94047268+Zen8688@users.noreply.github.com>
2021-11-10 15:39:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+let Discord = require(`discord.js`)
+require(`dotenv`).config();
+let client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
+
+['command_handler', 'event_handler'].forEach(handler => {
+    require(`./handlers/${handler}`)(client, Discord);
+})
+
+client.login(process.env.TOKEN);
